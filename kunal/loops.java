b46fd4bc02079c6c669f4533a6f650d768e3d372
saiprasad <saiprasad6687@gmail.com>
2025-09-01 19:08:37 +0530

    saiiii

--- kunal/loops.java
+++ kunal/loops.java
@@ -1,3 +1,20 @@
-/* syntax of if statements 
- * if boolean expression in t or f 
- */
+
+
+
+ public class Loops {
+    public static void main(String[] args) {
+
+        int salary = 25400;
+        if (salary > 10000) {
+            salary = salary + 2000;
+
+            
+        }
+        else {
+            salary = salary +1000;
+
+        }
+        System.out.println(salary);
+    }
+
+}
