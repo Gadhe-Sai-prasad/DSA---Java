11669d5548295e0dc4d13cfbcd5f2eafe08b0358
saiprasad <saiprasad6687@gmail.com>
2025-10-05 17:52:42 +0530

    chnages made

---
+++ Tap Academy/gcd.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class gcd {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int min = 0;
+
+        if(a<b) {
+            min = a;
+
+        }else {
+            min = b;
+        }
+
+        for (  int i = min ; i>=1; i--) {
+            
+            if(a%i==0 && b%i == 0) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+
+    
+}
