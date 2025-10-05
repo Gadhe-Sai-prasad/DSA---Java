a78b65eb976f5267eaf94c52cf3103c424b2de95
saiprasad <saiprasad6687@gmail.com>
2025-10-05 17:55:09 +0530

    perfect

---
+++ Tap Academy/ReverString.java
@@ -0,0 +1,21 @@
+public class ReverString {
+    static void reverse(int[] ar) {
+        int i = 0,j=ar.length-1;
+
+        while(i<j ) {
+            int t = ar[i];
+        
+            ar[i] = ar[j];
+            ar[j] = t;
+            i++;
+            j--;
+        }
+    }
+    
+    public static void main ( String[] args) {
+        int[] ar = {2,4,6,8,10,12,14};
+        for(int i = 0; i< arr.length; i++) {
+            System.out.println(ar[i]);
+        }
+    }
+}
